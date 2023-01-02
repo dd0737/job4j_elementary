db6b43f70bed89ab9bf5d0506d9c1f7f16b46011
sdgd0737 <d0737@ya.ru>
2023-01-02 09:36:48 +0300

    1. Модульные тесты. [#159435 #376655]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -16,6 +16,11 @@ public class ConverterTest {
 
     @Test
     public void whenConvert200RblThen3dot3333Dlr() {
+        float in = 200;
+        float expected = 3.3333f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
     }
 
     @Test
