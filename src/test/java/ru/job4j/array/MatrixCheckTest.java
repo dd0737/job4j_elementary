e657cd79e3a8b9f0c07c0b48774c42e664d844a9
sdgd0737 <d0737@ya.ru>
2023-01-11 09:09:30 +0300

    6.7.3. Массив из диагонали матрицы. [#214128]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -44,4 +44,34 @@ class MatrixCheckTest {
         };
         assertThat(MatrixCheck.monoVertical(input, 2)).isFalse();
     }
+
+    @Test
+    public void whenDiagonalFullX() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'},
+        };
+        assertThat(MatrixCheck.extractDiagonal(input)).containsExactly('X', 'X', 'X');
+    }
+
+    @Test
+    public void whenDiagonalFullOne() {
+        char[][] input = {
+                {'1', ' ', ' '},
+                {' ', '1', ' '},
+                {' ', ' ', '1'},
+        };
+        assertThat(MatrixCheck.extractDiagonal(input)).containsExactly('1', '1', '1');
+    }
+
+    @Test
+    public void whenDiagonalMix() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'Y', ' '},
+                {' ', ' ', 'Z'},
+        };
+        assertThat(MatrixCheck.extractDiagonal(input)).containsExactly('X', 'Y', 'Z');
+    }
 }
